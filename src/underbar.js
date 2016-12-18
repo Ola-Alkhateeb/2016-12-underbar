7d74e6a3955ab0aa67b086ebd3e14f7c90b4a01b
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2016-12-18 12:14:42 +0200

    part 1 Done

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,15 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if( n=== undefined) {
+      return array[array.length-1]
+    } else if(n===0){
+      return [] ; 
+    } else if(n>array.length){
+      return array ;}
+      else {
+      return array.slice(n-1 , array.length)
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +55,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(!Array.isArray(collection)){
+        for(var key in collection){
+          iterator(collection[key], key , collection)
+        }
+    }else {
+    for (var i = 0; i < collection.length; i++) {
+      iterator(collection[i],i , collection)
+     }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,24 +85,41 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var arr= [];
+    _.each(collection , function(elem,i){
+      if(test(elem,i)){
+        arr.push(elem)
+      }
+    })
+    return arr; 
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    return _.filter(collection , function(elem , i){
+       return !test(elem, i)
+    })
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var arr = [];
+    _.each(array , function(elem , i){
+      if(_.indexOf(arr,elem) === -1){
+        arr.push(elem);
+      }
+    })
+    return arr ; 
   };
 
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    var arr= [] ;
+    _.each(collection , function(elem , i){
+      arr.push(iterator(elem ,i))
+    })
+    return arr; 
   };
 
   /*
@@ -125,6 +161,14 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator===undefined){
+    accumulator=collection[0];
+   collection = collection.slice(1);
+    }
+      _.each(collection,function (elem){
+        accumulator=iterator(accumulator,elem)
+      })
+      return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -142,13 +186,30 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    // TIP: Try re-using reduce() here.
+    return _.reduce(collection , function(result,elem){
+      if(iterator === undefined){
+        return result && elem
+      }
+         if(!iterator(elem)){
+            result = false ;
+         }
+         return result ;
+    },true)
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
+    return _.reduce(collection , function(result,elem){
+      if(iterator === undefined){
+        return result || elem
+      }
+         if(iterator(elem)){
+            result = true ;
+         }
+         return result ;
+    },false)
+    //we want to try use every here if we had time :D 
   };
 
 
